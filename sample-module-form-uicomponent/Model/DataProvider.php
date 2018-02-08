6dd5efb6536247f458284f4cd6beb1860b8adb9b
Manuel García <manuel.garcia@ebolution.com>
2018-02-08 21:53:09 +0100

    Inject a dummy collection into an UI Form to avoid fatal errors performing collection actions

--- sample-module-form-uicomponent/Model/DataProvider.php
+++ sample-module-form-uicomponent/Model/DataProvider.php
@@ -5,11 +5,38 @@
  */
 namespace Magento\SampleForm\Model;
 
+use Magento\Ui\Model\ResourceModel\Bookmark\CollectionFactory;
+
 /**
  * Class DataProvider
  */
 class DataProvider extends \Magento\Ui\DataProvider\AbstractDataProvider
 {
+    protected $name;
+    protected $primaryFieldName;
+    protected $requestFieldName;
+    protected $meta;
+    protected $data;
+    protected $collection;
+
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        $meta = [],
+        $data = [],
+        CollectionFactory $bookmarkCollectionFactory
+        //IMPORTANT: please notice that we are injecting the class above as a dummy collection due to a bug that forces
+        //data providers to content a valid collection
+    ) {
+        $this->name = $name;
+        $this->primaryFieldName = $primaryFieldName;
+        $this->requestFieldName = $requestFieldName;
+        $this->meta = [];
+        $this->data = [];
+        $this->collection = $bookmarkCollectionFactory->create();
+    }
+
     /**
      * Get data
      *
